c2b089bb2e987a6619e2125492b1ee2b4e7840cf
railstutorialer <railstutorialer15@gmail.com>
2016-08-15 15:14:36 +0200

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,4 +10,48 @@ describe "User pages" do
 
     	it {should have_selector 'title', text: (full_title 'Sign up')}
   	end
+
+  	describe 'profile page' do
+  		let :user do
+  			FactoryGirl.create :user
+  		end
+  		before { visit user_path user }
+
+  		it { should have_selector 'h1', text: user.name }
+  		it { should have_selector 'title', text: user.name }
+  	end
+
+  	describe 'signup' do
+  		before do
+  			visit signup_path
+  		end
+
+  		let(:submit) { 'Create my account' }
+
+  		describe 'with invalid information' do
+  			it 'should not create a user' do
+  				result = expect do
+  					click_button submit
+  				end
+  				result.not_to change User, :count
+  			end
+  		end
+
+  		describe 'with valid information' do
+  			before do
+  				fill_in 'Name', with: 'Example User'
+  				fill_in 'Email', with: 'user@example.com'
+  				fill_in 'Password', with: 'foobar'
+  				fill_in 'Confirmation', with: 'foobar'
+  			end
+
+  			it 'should create a user' do
+  				result = expect do
+  					click_button submit
+  				end
+  				result.to (change User, :count).by 1
+  			end
+  		end
+  	end
+
 end
