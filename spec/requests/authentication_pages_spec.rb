230a7f48b2c491b5302921ff8d1cc84f60d3e7d8
railstutorialer <railstutorialer15@gmail.com>
2016-08-23 21:13:07 +0200

    Finish user edit, update, index and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -40,11 +40,13 @@ describe "Authentication" do
 			end
 
 			before do
-				valid_signin user
+				sign_in user
 			end
 
 			it { should have_selector 'title', text: user.name }
+			it { should have_link 'Users', href: users_path }
 			it { should have_link 'Profile', href: (user_path user) }
+			it { should have_link 'Settings', href: (edit_user_path user) }
 			it { should have_link 'Sign out', href: signout_path }
 			it { should_not have_link 'Sign in', href: signin_path }
 
@@ -54,6 +56,105 @@ describe "Authentication" do
 				end
 
 				it { should have_link 'Sign in'}
+			end
+		end
+	end
+
+	describe 'authorization' do
+		describe 'for non-signed-in users' do
+			let :user do
+				FactoryGirl.create :user
+			end
+
+			describe 'in the Users controller' do
+				describe 'visiting the edit page' do
+					before do
+						visit (edit_user_path user)
+					end
+
+					it { should have_selector 'title', text: 'Sign in' }
+				end
+
+				describe 'submitting to the update action' do
+					before { put (user_path user)}
+					specify { response.should redirect_to signin_path}
+				end
+			end
+		end
+
+		describe 'as wrong user' do
+			let :user do
+				FactoryGirl.create :user
+			end
+
+			let :wrong_user do
+				FactoryGirl.create :user, email: 'wrong@example.com'
+			end
+
+			before do
+				sign_in user
+			end
+
+			describe 'visiting Users#edit page' do
+				before { visit edit_user_path wrong_user }
+				it { should_not have_selector 'title', text: (full_title 'Edit user') }
+			end
+
+			describe 'submitting a PUT request to the Users#update action' do
+				before {put (user_path wrong_user)}
+				specify { response.should redirect_to root_path }
+			end
+		end
+
+		describe 'for non signed-in users' do
+			let :user do
+				FactoryGirl.create :user
+			end
+
+			describe 'when attempting to visit a protected page' do
+				before do
+					visit edit_user_path user
+					fill_in 'Email', with: user.email
+					fill_in 'Password', with: user.password
+					click_button 'Sign in'
+				end
+
+				describe 'after signing in' do
+					it 'should render the desired protected page' do
+						page.should have_selector 'title', text: 'Edit user'
+					end
+				end
+			end
+
+			describe 'visiting the user index' do
+				before do
+					visit users_path
+				end
+				it { should have_selector 'title', text: 'Sign in' }
+			end
+		end
+
+		describe 'as non-admin user' do
+			let :user do
+				FactoryGirl.create :user
+			end
+
+			let :non_admin do
+				FactoryGirl.create :user
+			end
+
+			before do
+				sign_in non_admin
+			end
+
+			describe 'submitting a DELETE request to the Users#destroy action' do
+				before do 
+					delete user_path user 
+				end
+
+				specify do
+					response.should redirect_to root_path 
+				end
 			end
 		end
 	end
